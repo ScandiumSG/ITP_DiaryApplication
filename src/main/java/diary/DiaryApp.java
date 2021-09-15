6195889b683f1bb8bee66c2e00a99842550adb05
Jakob <jakoblien01@gmail.com>
2021-09-15 22:34:02 +0200

    Opprettet skjelett
    Opprettet et Skjelett for videre arbeid basert på fleir ulike kilda. No kjøre testa(selv om det er ingen) og appen referere te FXML fil for å lag vindu:)

---
+++ src/main/java/diary/DiaryApp.java
@@ -0,0 +1,25 @@
+package diary;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class DiaryApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("Diary.fxml"));
+        Parent parent = fxmlLoader.load();
+        stage.setScene(new Scene(parent));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
