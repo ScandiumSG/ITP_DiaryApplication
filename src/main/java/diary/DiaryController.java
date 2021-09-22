082759a3c3bcd3abe0fd178be12c8f8a4e632de5
Lars Overskeid <larsover@stud.ntnu.no>
2021-09-22 12:25:53 +0000

    Update src/main/java/diary/DiaryController.java, src/main/resources/diary/Diary.fxml files

---
+++ src/main/java/diary/DiaryController.java
@@ -0,0 +1,36 @@
+package diary;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DiaryController {
+
+    @FXML
+    private TextField textEntry;
+
+    @FXML
+    private Label dateId;
+
+    @FXML
+    private Button dateSubmit;
+
+    @FXML
+    private Button entrySubmit;
+
+    @FXML
+    private DatePicker dateInput;
+
+    @FXML
+    public saveDateEntry(ActionEvent event) {
+        return;
+    }
+
+    @FXML
+    public retrieveDateEntry(ActionEvent event) {
+        return;
+    }
+
+}
