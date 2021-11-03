9b80904d613e177bd3efb7f1efdf122dd730a4c8
Stian Kjønniksen Gaustad <stiankg@stud.ntnu.no>
2021-11-03 13:29:27 +0000

    EntrySearch - New EntrySearch class and associated JUnit

---
+++ diary/core/src/test/java/diary/core/EntrySearchTest.java
@@ -0,0 +1,55 @@
+package diary.core;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import diary.json.EntryToJSON;
+
+public class EntrySearchTest {
+    private String fileName;
+    private File Testfile;
+
+    @BeforeEach
+    public void makeFile() throws IOException {
+        String fileName = "SearchTest";
+        Testfile = EntryToJSON.getJsonFile(fileName);
+        this.fileName = fileName;
+        Entry entry1 = new Entry("potato tomato", "01-01-2011");
+        EntryToJSON.write(fileName, entry1);
+        Entry entry2 = new Entry("potato tomato tornado", "02-01-2011");
+        EntryToJSON.write(fileName, entry2);
+        Entry entry3 = new Entry("tomato", "03-01-2011");
+        EntryToJSON.write(fileName, entry3);
+        Entry entry4 = new Entry("tomato tornado tobago", "04-01-2011");
+        EntryToJSON.write(fileName, entry4);
+    }
+
+    @Test
+    public void singleKeywordTest() throws IOException {
+        String keyword1 = "tomato";
+        String keyword2 = "potato";
+        String keyword3 = "tornado";
+        String keyword4 = "	cato";
+
+        Assertions.assertTrue(
+            EntrySearch.searchEntries(
+                fileName, keyword1).size() == 4);
+        Assertions.assertTrue(
+            EntrySearch.searchEntries(
+                fileName, keyword1, keyword2).size() == 2);
+        Assertions.assertTrue(
+            EntrySearch.searchEntries(
+                fileName, keyword1, keyword2, keyword3).size() == 3);
+        Assertions.assertTrue(
+            EntrySearch.searchEntries(
+                fileName, keyword4).size() == 0);
+    }
+
+    @AfterEach
+    public void deleteFile() {
+        Testfile.delete();
+    }
+}
