d3897a650de56dc07f60fe96c1c557d95de94ec4
Sebastian Veum <sebasv@stud.ntnu.no>
2021-11-07 19:15:17 +0000

    removed property

--- diary/ui/src/main/java/diary/ui/DiaryApp.java
+++ diary/ui/src/main/java/diary/ui/DiaryApp.java
@@ -14,9 +14,7 @@ public class DiaryApp extends Application {
         System.setProperty("testfx.headless", "true");
         System.setProperty("prism.order", "sw");
         System.setProperty("prism.text", "t2k");
-        System.setProperty("java.awt.headless", "true");
-        System.setProperty("glass.platform", "monocle")
-        
+        System.setProperty("java.awt.headless", "true");        
     }
 
     @Override
