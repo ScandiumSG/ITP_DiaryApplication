2ede889749984d7d6dcd2deac70c0ec08a7673e0
Sebastian Veum <sebasv@stud.ntnu.no>
2021-11-26 20:19:42 +0000

    test - changed variable name

--- diary/core/src/test/java/diary/core/EntrySearchTest.java
+++ diary/core/src/test/java/diary/core/EntrySearchTest.java
@@ -12,7 +12,7 @@ import diary.json.PersistanceUtil;
 
 public class EntrySearchTest {
     private String fileName;
-    private File Testfile;
+    private File testfile;
     private User user = new User("TestPerson", "4252");
 
     /**
@@ -23,7 +23,7 @@ public class EntrySearchTest {
     @BeforeEach
     public void makeFile() throws IOException {
         String fileName = "SearchTest";
-        Testfile = PersistanceUtil.getJsonFile(user, fileName);
+        testfile = PersistanceUtil.getJsonFile(user, fileName);
         this.fileName = fileName;
         Entry entry1 = new Entry("potato tomato", "01-01-2011");
         EntryToJSON.write(user, fileName, entry1);
@@ -66,6 +66,6 @@ public class EntrySearchTest {
      */
     @AfterEach
     public void deleteFile() {
-        Testfile.delete();
+        testfile.delete();
     }
 }
